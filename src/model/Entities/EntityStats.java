c94bffeec1bcc2de65327a410985971095348612
Danilo Souza <danilos063@gmail.com>
2018-04-17 14:48:17 -0400

    Put Entity and EntityStats in the right package

--- src/model/Entities/EntityStats.java
+++ src/model/Entities/EntityStats.java
@@ -3,23 +3,75 @@ package model.Entities;
 import model.Map.Direction;
 
 public class EntityStats {
-    private int maxHealth;
-    private int currentHealth;
-    private int level;
-    private int experience;
-    private int defense;
-    private int detectRange;
-    private int maxSpeed;
-    private int currentSpeed;
-    private Direction facingDirection;
-
-    private int visibleRadius;
-
-    public EntityStats(){
-
-    }
-
-    public int getCurrentSpeed(){
-        return currentSpeed;
-    }
+	private int maxHealth;
+	private int currentHealth;
+	private int level;
+	private int experience;
+	private int defense;
+	private int detectRange;
+	private int maxSpeed;
+	private int currentSpeed;
+	private Direction facingDirection;
+	private Direction derivedDirection;
+	
+	public EntityStats() {
+		maxHealth = 100;
+		currentHealth = 100;
+		level = 1;
+		defense = 10;
+		detectRange = 10;
+		maxSpeed = 5;
+		currentSpeed = 0;
+		//facingDirection = new Direction(N);
+	}
+	
+	public void modifyHealth(int delta) {
+		currentHealth += delta;
+		
+		if(currentHealth < 0) {
+			currentHealth = 0;
+		}
+		
+		if(currentHealth > maxHealth) {
+			currentHealth = maxHealth;
+		}
+	}
+	
+	/*public void modifyHealth(int delta, Time time) {
+		
+	}*/
+	
+	public void modifyMaxHealth(int delta) {
+		maxHealth += delta;
+		
+		if(maxHealth < 0) {
+			maxHealth = 0;
+		}
+	}
+	
+	public void modifyDefense(int delta) {
+		defense += delta;
+		
+		if(defense < 0) {
+			defense = 0;
+		}
+	}
+	
+	public void levelUp() {
+		++level;
+		
+		experience = 0;
+	}
+	
+	public void modifyExperience(int delta) {
+		experience += delta;
+		
+		if(experience < 0) {
+			experience = 0;
+		}
+		
+		if(experience == 100) {
+			levelUp();
+		}
+	}
 }
