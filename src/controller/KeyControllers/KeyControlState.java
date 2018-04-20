3bebc87037f8aab2743e2bcb37d2042c8392008b
Danilo Souza <danilos063@gmail.com>
2018-04-20 16:59:49 -0400

    Created KeyControlState

--- src/controller/KeyControllers/KeyControlState.java
+++ src/controller/KeyControllers/KeyControlState.java
@@ -6,6 +6,29 @@ public class KeyControlState {
 	private HashMap<String, KeyController> controllers;
 	private KeyController activeController;
 	//private KeyBindingController keyBindings;
-
 	
+	public KeyControlState() {
+		activeController = controllers.get("player");
+	}
+
+	private void SwapControllerTo(KeyController controller) {
+		activeController = controller;
+	}
+	
+	public void swapToCamera() {
+		SwapControllerTo(controllers.get("camera"));
+	}
+	
+	public void swapToInventory() {
+		SwapControllerTo(controllers.get("inventory"));
+	}
+	
+	public void swapToPlayer() {
+		SwapControllerTo(controllers.get("player"));
+	}
+	
+	//TODO change the name of the variables passed into the parameter
+	public void changeKeyCode(int x, int y) {
+		
+	}
 }
