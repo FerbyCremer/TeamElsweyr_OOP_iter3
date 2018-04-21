3654eef73876304653e508b52b0b11e4eafa7d63
zscrib0619 <zscrib0619@ufl.edu>
2018-04-21 16:38:39 -0400

    NPC edit

--- src/model/Entities/NPC/NPC.java
+++ src/model/Entities/NPC/NPC.java
@@ -1,13 +1,15 @@
 package model.Entities.NPC;
 
+import controller.EntityControllers.AIController;
 import model.Entities.AI;
 import model.Entities.Player;
 
 public class NPC extends AI {
     private NPCState npcState;
 
-    public NPC(NPCState npcState){
+    public NPC(NPCState npcState, AIController aiController){
         this.npcState = npcState;
+        this.aiController = aiController;
     }
     @Override
     public void move() {
