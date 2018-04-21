310dc1be7eb15985d5af9e6f27b1052327bb4cc5
Danilo Souza <danilos063@gmail.com>
2018-04-21 11:34:52 -0400

    Added EntityStats and deadHandler to Mount and Pet

--- src/model/Entities/Pet.java
+++ src/model/Entities/Pet.java
@@ -1,10 +1,13 @@
 package model.Entities;
 
+import controller.Handlers.BringOutYourDeadHandler;
+
 public class Pet extends Entity {
 
     private Player owner;
 
-    public Pet(Player player){
+    public Pet(Player player, EntityStats stats, BringOutYourDeadHandler deadHandler){
+    	super(stats, deadHandler);
         owner = player;
     }
 }
