26543ca3c7dcaa989f5e938fadf3d9333f19a25c
zscrib0619 <zscrib0619@ufl.edu>
2018-04-23 03:43:49 -0400

    stuff

--- src/view/ZoneView.java
+++ src/view/ZoneView.java
@@ -45,6 +45,7 @@ public class ZoneView {
             for(int x = 0; x < decalSet.getXDim(); x++){
             ArrayList<String> tempList = decalSet.getTileContents(new Point(x, y));
                 for (String s : tempList) {
+                    System.out.println(s);
                     gc.drawImage(sprites.getImage(s), hexX, hexY, hexWidth, hexHeight);
                 }
                 hexY+= hexHeight;
